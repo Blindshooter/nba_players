f4ad12dd1a2555983297ae7d545bf421178f1600
Blindshooter <alexanderruppelt@gmail.com>
2017-12-11 09:40:15 +0200

    wip

--- nba_players/spiders/players_stat.py
+++ nba_players/spiders/players_stat.py
@@ -1,5 +1,9 @@
 import scrapy
-
+from datetime import datetime
+from datetime import timedelta
+#from pytz import timezone
+import re
+import pandas as pd
 
 
 
@@ -9,39 +13,39 @@ import sys
 
 # get me all active players
 
-url_allPlayers = ("http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason"
-		"=0&LeagueID=00&Season=2015-16")
+# url_allPlayers = "http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=0&LeagueID=00&Season=2017-18"
 
-#request url and parse the JSON
-response = requests.get(url_allPlayers)
-response.raise_for_status()
-players = response.json()['resultSets'][0]['rowSet']
+# request url and parse the JSON
+# response = requests.get(url_allPlayers)
+# response.raise_for_status()
+# players = response.json()['resultSets'][0]['rowSet']
 
 # use roster status flag to check if player is still actively playing
-active_players = [players[i] for i in range(0,len(players)) if players[i][2]==1 ]
+# active_players = [players[i] for i in range(0,len(players)) if players[i][2]==1 ]
 
-ids = [active_players[i][0] for i in range(0,len(active_players))]
+# ids = [active_players[i][0] for i in range(0,len(active_players))]
 
-print("Number of Active Players: " + str(len(ids)))
+# print("Number of Active Players: " + str(len(ids)))
 
-### https://gist.github.com/timmyshen/32d682c7b8aef014c256
+# https://gist.github.com/timmyshen/32d682c7b8aef014c256
 
 
 class PlayerStats(scrapy.Spider):
-    name = "nba_palayer_stats"
-    start_urls = [
-        'http://stats.nba.com/players/list/',
-    ]
+    name = "nba_player_stats"
+    url_begin = ''
+
+    # start_urls = [ url_begin+id for id in ids ]
+    start_urls = ['http://stats.nba.com/player/203518/', 'http://stats.nba.com/player/203084/']
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+        for url in url_players:#in response.css('div.quote'):
+            yield scrapy.Request(url, callback=self.parse_player_stats)
 
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # next_page = response.css('li.next a::attr(href)').extract_first()
+        # if next_page is not None:
+            # next_page = response.urljoin(next_page)
+            # yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_player_stats(self, response):
+        item = NbaPlayersItem()
+        item['name'] = response.xpath().extract()
